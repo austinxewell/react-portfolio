b54bf7b217ca4645ecb92367ca15660c39f795f1
AustinxEwell <austin.ewell86@gmail.com>
2022-01-02 17:18:21 -0700

    fixed contact form. contact form now working as intended.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,16 @@
 import React, { useState } from 'react';
-import { FormEvent } from 'react';
 import { validateEmail } from '../../utils/helpers';
 import axios from 'axios';
 
 function Contact() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
   const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      setFormState({ [e.target.name]: e.target.value });
-      console.log('Form', formState);
-    }
-  };
+  var { name, email, message } = formState;
 
   const handleChange = (e) => {
     if (e.target.name === 'email') {
       const isValid = validateEmail(e.target.value);
+      email = e.target.value
       if (!isValid) {
         setErrorMessage('Your email is invalid!');
       } else {
@@ -31,19 +23,27 @@ function Contact() {
         setErrorMessage('');
       }
     }
+    if(e.target.name === 'name'){
+      name = e.target.value
+    }
+    if(e.target.name === 'message'){
+      message = e.target.value
+    }
   };
+  
+  
 
   const formId = 'zMe83qAe';
   const formSparkUrl = `https://submit-form.com/${formId}`
 
-  const submitForm = async (event: FormEvent) => {
+  const submitForm = async (event) => {
     event.preventDefault();
     await postSubmission();
   };
 
   const postSubmission = async () => {
     const payload = {
-      message: 'Test formspark submission',
+      ...{'name': name, 'email': email, 'message': message}
     };
 
     try{
@@ -55,12 +55,12 @@ function Contact() {
   };
 
   return (
-    <section className='contactCard'>      <div className='mx-2'>
+    <section className='contactCard'>
+      <div className='mx-2'>
         <h2>Reach me Directly At:</h2>
         <a href='mailto:austin.ewell86@gmail.com' className='emailLink'>austin.ewell86@gmail.com</a>
         <p>(801) 231-1314</p>
         <h2>- OR -</h2>
-        <p>(For display purposes only)</p>
       </div>        
         <fieldset>
             <legend>
@@ -69,15 +69,15 @@ function Contact() {
             <form className="contactForm" id="contact-form" onSubmit={submitForm}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label><br/>
-                    <textarea className="messageText" name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+                    <textarea className="messageText" name="message" rows="5" defaultValue={message} onChange={handleChange} />
                 </div>
                 {errorMessage && (
                     <div>
